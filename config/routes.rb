16abc560d395e4df323d10acc045381645df83f3
Ueverton-oliveira <ueverton.souz@gmail.com>
2021-06-11 16:29:27 -0400

    add controllers and viwes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
+  get 'patients/index'
+  get 'doctors/index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
