ef8e37728389391662d72165ef8c79431fc9b0ef
Ueverton-oliveira <ueverton.souz@gmail.com>
2021-06-17 20:04:05 -0400

    action appointment

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -2,8 +2,28 @@ class AppointmentsController < ApplicationController
   layout 'appointments'
 
   def index
+    @appointments = Appointment.all.page(params[:page]).per(10)
   end
 
   def new
+    @appointment = Appointment.new
+  end
+
+  def create
+    @appointment = Appointment.new(params_appointment)
+    if @appointment .save!
+      redirect_to  appointments_path, notice: "Médico criado com sucesso!"
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  private
+
+  def params_appointment
+    params.require(:appointment).permit(:starts_at, :ends_at)
   end
 end
